5872e974cd148d404b7134b0946d4e89f5a13698
sergeyschegolev89 <lis089@mail.ru>
2022-10-26 15:50:58 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <p>test</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+
+  }
+});
+</script>
+
+
